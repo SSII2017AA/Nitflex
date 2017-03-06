0d5a6470272ba72d0dbe2a16c54d2a09c2f0d1e0
alvaro <alvaro@alvaro-PC>
2017-03-06 19:10:45 +0100

    --------------------------------BACKEND----------------------------
    Inicialización del proyecto
    - Estado: 0%

---
+++ src/main/java/com/nitflex/NitflexBeApplication.java
@@ -0,0 +1,12 @@
+package com.nitflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NitflexBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NitflexBeApplication.class, args);
+	}
+}
